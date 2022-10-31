819a8d45ff5e9dd8f12ab7bba195a44638b9a808
ArzuGoyushova <arzusg@code.edu.az>
2022-10-31 21:17:34 +0400

    feat: Exercise1 is added.

---
+++ ArrayExercise1_reverse.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Print array elements in reverse order
+
+int[] nums = { 1, 3, 6, 4, 5, 7, 9 };
+int[] numsReversed = new int[10]; 
+
+
+for (int i = nums.Length-1; i>=0; i--)
+{
+    numsReversed[i] = nums[i];
+
+    Console.WriteLine(numsReversed[i]);
+}
