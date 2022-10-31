164da40e3cd7f165a021b5174834c4d73555a2fa
ArzuGoyushova <arzusg@code.edu.az>
2022-10-31 21:19:45 +0400

    feat:Exercise4 is added.

---
+++ ArrayExercise4_MinMax.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Find max and min in array
+
+int[] arr = { 2, 7, 8, 12, 3, 16, 5 };
+int max = arr[0];
+int min = arr[0];
+
+for(int i = 0; i < arr.Length; i++)
+{
+    if (max < arr[i])
+    {
+        max= arr[i];
+    }
+    if (min > arr[i])
+    {
+        min = arr[i];
+    }
+}
+Console.WriteLine(max);
+Console.WriteLine(min);
