482922c020cfd85d2e8edc81392f8b68b6f35e99
ArzuGoyushova <arzusg@code.edu.az>
2022-11-01 17:57:58 +0400

    feat: ArrayExercise7 is added.

---
+++ ArrayExercise7_DescendingOrder.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Descending order.
+
+using System.Globalization;
+
+int[] arr = { 5, 9, 4, 2, 3, 12 };
+int temp;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j=i+1; j<arr.Length; j++)
+    {
+        if (arr[j] > arr[i])
+        {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i]);
+}
