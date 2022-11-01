6ffdc20c9f13436ea585cf5500de0e1cb35a8e9e
ArzuGoyushova <arzusg@code.edu.az>
2022-11-01 17:57:04 +0400

    feat: ArrayExercise5 is added.

---
+++ ArrayExercise5_OddEven.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//separate odd and even integers in separate arrays. 
+
+using System.Globalization;
+
+int[] arr1 = { 2, 5, 6, 8, 9 };
+int[] arrOdd = new int[5];
+int[] arrEven = new int[5];
+int j = 0, k = 0;
+for (int i=0; i<arr1.Length; i++)
+{
+    if (arr1[i]%2==1)
+    {
+        arrOdd[j] = arr1[i];
+        j++;
+    }
+    else
+    {
+        arrEven[k] = arr1[i];
+        k++;
+    }
+    
+}
+for (int i=0;i<j; i++)
+{
+    Console.WriteLine($"Odd numbers = {arrOdd[i]}");
+}
+for (int i=0; i<k; i++)
+{
+    Console.WriteLine($"Even numbers = {arrEven[i]}");
+}
