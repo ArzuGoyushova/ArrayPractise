30af92e6f6c915959fc76225b6b6555c564f77cd
ArzuGoyushova <arzusg@code.edu.az>
2022-11-04 20:21:16 +0400

    feat: Exercise 12 is added.

---
+++ ArrayExercise12_SecondLargest.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] arr = { 1, 2, 4, 6, 7, 9 };
+int i, j=0;
+int lrg = arr[0];
+int lrg2 = arr[0];
+
+for (i = 0; i < arr.Length; i++)
+{
+    if (lrg < arr[i])
+    {
+        lrg = arr[i];
+        j = i;
+    }
+}
+for (i = 0; i < arr.Length; i++)
+{
+    if (i == j)
+    {
+        i++;
+        i--;
+    }
+    else
+    {
+        if (lrg2 < arr[i])
+        {
+            lrg2 = arr[i];
+        }
+    }
+}
+Console.WriteLine(lrg2);
