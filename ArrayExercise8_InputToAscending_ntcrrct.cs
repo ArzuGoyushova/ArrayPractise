1bf458d50819ab81fe386afc879939aabf8342e6
ArzuGoyushova <arzusg@code.edu.az>
2022-11-04 20:19:16 +0400

    feat: Exercise 8 is added.

---
+++ ArrayExercise8_InputToAscending_ntcrrct.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Input a number to the ascending sorted list.
+
+int[] arr = { 1, 2, 3, 4, 6, 7 };
+int input = 5;
+int p=4;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (input < arr[i])
+    {
+        break;
+        p = i;
+    }
+    else
+    {
+        p = i + 1;
+    }
+}
+
+    //saga duzmek
+    for (int i = arr.Length + 1; i >= p; i--)
+    {
+        arr[i] = arr[i - 1];
+        arr[p] = input;
+    }
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
