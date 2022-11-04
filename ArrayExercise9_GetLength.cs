7fdab2b5f945d2fd83e314cdfa0d0140bcfb5895
ArzuGoyushova <arzusg@code.edu.az>
2022-11-04 20:20:05 +0400

    feat: Exercise 9 is added.

---
+++ ArrayExercise9_GetLength.cs
@@ -0,0 +1,12 @@
+﻿https://aka.ms/new-console-template for more information
+
+int[,] arr = {{ 5, 8, 7 },{ 21, 1, 3 },{ 12, 2, 3 }};
+int sum = 0;
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+            sum = sum + arr[i, i];
+}
+Console.WriteLine(sum);
+	  
+
