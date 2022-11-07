6770611f6a1756175614049728a426bfe3ea5e6c
Arzu <115929474+ArzuGoyushova@users.noreply.github.com>
2022-11-07 19:42:58 +0400

    feat: Method

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Divided(42));
+        }
+        static bool Divided(int n)
+        {
+            return n % 21 == 0;
+
+        }
+    }
+}
